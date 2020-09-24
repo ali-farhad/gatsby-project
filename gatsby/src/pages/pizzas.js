7a6362c25165681e71f72a944a6e48a56fd7ef39
farhad-desktop <alifarhad557@gmail.com>
2020-09-24 07:57:34 -0700

    init repo

---
+++ gatsby/src/pages/pizzas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function PizzasPage() {
+  return (
+    <div>
+      <Nav />
+      This is Pizzas Page
+    </div>
+  );
+}
